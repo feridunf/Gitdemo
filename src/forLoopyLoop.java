9c65f62388ac413db9e8a442e256ecfab068f457
feridunf <ffangaj@fordham.edu>
2022-01-01 00:41:27 -0500

    test 1

---
+++ src/forLoopyLoop.java
@@ -0,0 +1,20 @@
+public class forLoopyLoop {
+
+    public static void main(String[] args){
+
+        for (int i = 2; i < 3; i++){
+            System.out.println(i);
+        }
+    }
+
+}
+
+/* three statements
+one declares the variable, one deals with the parameter or limiter, and one iterates
+
+Statement 1 is executed (one time) before the execution of the code block. (int i = 2)
+
+Statement 2 defines the condition for executing the code block. ( i < 3 )
+
+Statement 3 is executed (every time) after the code block has been executed. ( i ++ )
+ */
